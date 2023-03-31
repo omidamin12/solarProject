be0a9fc4db22595df0c42d5031c51e00445d593e
omidamin12 <fnuomied@gmail.com>
2023-03-31 13:12:03 -0400

    main

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        dog dog=new dog();
+        dog.name="tom";
+        dog.age=14;
+        dog.color="red";
+
+    }
+}
---
+++ src/dog.java
@@ -0,0 +1,6 @@
+public class dog {
+    String name;
+    int age;
+    String color;
+
+}
